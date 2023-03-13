48a441183d22ce80971795bb68da5f21b938f896
marianapequeno <marianapqueno@gmail.com>
2023-03-13 12:03:23 -0300

    Placing pieces on the board

--- src/boardgame/Board.java
+++ src/boardgame/Board.java
@@ -20,7 +20,8 @@ public class Board {
     }
 
     public void placePiece(Piece piece, Position position) {
-
+        pieces[position.getRow()][position.getColumn()] = piece;
+        piece.position = position;
     }
 
 //    public Piece removePiece(Position position) {
